acab00a78ffc90fd8eb0974ef6f156c049cc3756
ali 2236 <ali.gh2236@gmail.com>
2021-10-02 14:41:11 +0330

    menu color

--- app_bar.tsx
+++ app_bar.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 
 type AppBarProps = {
-    title? : String,
-    actions? : React.ReactNode
+    title?: string,
+    actions?: React.ReactNode,
+    menuColor: string,
 }
 
-export default function AppBar({ title, actions } : AppBarProps){
+export default function AppBar({ title, actions, menuColor }: AppBarProps) {
+
+    const menuIcon = (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M21 18H12V16H21V18ZM21 13H3V11H21V13ZM21 8H3V6H21V8Z" fill={menuColor ?? "#fff"} />
+        </svg>
+    );
+
     return (
-    <div className="flex flex-row pl-8">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">{title}</h1>
-        <div className="flex-grow"></div>
-        <div className="flex-row hidden lg:flex xl:mr-16">
-            {actions}
+        <div className="flex flex-row pl-8">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">{title}</h1>
+            <div className="flex-grow"></div>
+            <div className="flex flex-row mr-4 lg:hidden">{menuIcon}</div>
+            <div className="flex-row hidden lg:flex xl:mr-16">
+                {actions}
+            </div>
         </div>
-    </div>
     );
 }
